a8423f942cec385c7c467e941d8b9ebd051d19e6
nebahabesha <nebiyu28@gmail.com>
2021-07-04 01:01:44 +0300

    speed converter exercises

--- src/myClass.java
+++ src/myClass.java
@@ -1,15 +1,26 @@
 public class myClass {
     public static void main(String[] args) {
-toMilesPerHour(1.5);
-    }
-    public static long toMilesPerHour(double kilometersPerHour) {
 
-        if (kilometersPerHour < 0) {
-            return -1;
+      printConversion(12.56);
+    }
+        public static long toMilesPerHour(double kilometersPerHour) {
+            long milesPerHour;
+            if (kilometersPerHour < 0) {
+                milesPerHour = -1;
+            } else {
+                //1 miles per hour is 1.609 kms per hour.
+                milesPerHour = Math.round(kilometersPerHour / 1.609);
+            }
+
+            return milesPerHour;
         }
-        return Math.round(kilometersPerHour/1.609);
 
-
-
+        public static void printConversion(double kilometersPerHour) {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            if (milesPerHour == -1) {
+                System.out.println("Invalid Value");
+            } else {
+                System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+            }
+        }
     }
-}
